b8e5023986fcc33410d5490132627fedbf4cb149
s-uchidam <s-uchidam@53S50031122.it.bsd405.org>
2019-09-20 13:24:56 -0700

    Turning in project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -14,14 +14,46 @@
  *
  */
 
-// YOU CODE HERE :) 
 
 public class ASCIIArt {
+	public static void MessageBox() {
+		System.out.println(" A_A_A_A_A_A_A_A");
+		System.out.print("< ");
+		Message();
+		System.out.println(" >");
+		System.out.println(" Y^Y^Y^Y^Y^Y^Y^Y");
+	}
+	public static void Message() {
+		System.out.print("This is a pen");
+	}
+	public static void Pencil() {
+		System.out.println("\t      A");
+		System.out.println("\t     / \\");
+		System.out.println("\t    /   \\");
+		System.out.println("\t   /     \\");
+		System.out.println("\t   |VVVVV|");
+		Body();
+		Body();
+		Body();
+		System.out.println("\t   |     |");
+		System.out.println("\t   | No2 |");
+		System.out.println("\t   |_____|");
+		System.out.println("\t   |||||||");
+		System.out.println("\t   [_____]");
+	}
+	public static void Statement() {
+		System.out.println("\nVery basic, old-school style, no.2 pencil.");
+	}
+	public static void Body() {
+		System.out.println("\t   |  O  |");
+		System.out.println("\t   |  O  |");
+		System.out.println("\t   |  O  |");
+		System.out.println("\t   |  O  |");
+		System.out.println("\t   |  O  |");
+	}
 	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+		MessageBox();
+		Pencil();
+		Statement();
 	}
 }
